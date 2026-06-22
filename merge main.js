c70742d47c16ac5043bff2d8dd365932882876f2
danialzivehdar <243804543+danialzivehdar1992-hue@users.noreply.github.com>
2026-06-22 22:40:33 +0330

    Create merge main.js

---
+++ merge main.js
@@ -0,0 +1,24 @@
+configuration>
+<paths>
+<alias  id="AliasID"  path="Aliaspath" />
+<using  namespace="Namespace "/>
+</paths>
+<modules>
+<module  id="ModuleID"  class="ModuleClass" PropertyName
+.../>
+</modules>
+<authorization>
+39
+Chaptr  3.  Configurations
+<allow pages="PageID1,PageID2" Users="User1,Uses2"
+Vers="get" />
+<deny pages="PageID1,PageID2"  users="Use1,Use2" roIes="Roles,Rles2" verb="post" />
+</authorization>
+<pages  PropertyName="PropertyValus"...>
+<page  id="PageID" PropertyName="PropertyValue".../>
+</parameters>
+<parameter id="ParameterID" class="parameterClass"
+PropertyName="PropertyValus".../>
+</parameters>
+</configuration>
+﻿
